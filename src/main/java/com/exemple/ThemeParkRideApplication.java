c891ee5e9240f1fb245beb6e21e2183f992c4d0e
Ben JAMAA Soufiene <benjamaasoufiene@gmail.com>
2021-01-23 09:33:14 +0100

    Creating a Spring Boot application

---
+++ src/main/java/com/exemple/ThemeParkRideApplication.java
@@ -0,0 +1,27 @@
+package com.exemple;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.exemple.model.ThemeParkRide;
+import com.exemple.repository.ThemeParkRideRepository;
+
+@SpringBootApplication
+public class ThemeParkRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeParkRideApplication.class);
+	}
+
+	@Bean
+	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+		return (args) -> {
+			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+		};
+	}
+
+}
---
+++ src/test/java/com/exemple/ThemeParkRideApplicationTests.java
@@ -0,0 +1,48 @@
+package com.exemple;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+class ThemeParkRideApplicationTests {
+
+	 @Autowired
+	    private MockMvc mockMvc;
+	    @Test
+	    public void getsAllRides() throws Exception {
+	        mockMvc.perform(MockMvcRequestBuilders.get("/ride")
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk())
+	                .andReturn();
+	    }
+	    @Test
+	    public void getsSingleRide() throws Exception {
+	        mockMvc.perform(MockMvcRequestBuilders.get("/ride/1")
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk())
+	                .andReturn();
+	    }
+	    @Test
+	    public void returnsNotFoundForInvalidSingleRide() throws Exception {
+	        mockMvc.perform(MockMvcRequestBuilders.get("/ride/4")
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isNotFound())
+	                .andReturn();
+	    }
+	    @Test
+	    public void addsNewRide() throws Exception {
+	        String newRide = "{\"name\":\"Monorail\",\"description\":\"Sedate travelling ride.\",\"thrillFactor\":2,\"vomitFactor\":1}";
+	        mockMvc.perform(MockMvcRequestBuilders.post("/ride")
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .content(newRide)
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk())
+	                .andReturn();
+	    }
+}
