e804b8075f681a0c8b98326a47982435185236ea
Ben JAMAA Soufiene <benjamaasoufiene@gmail.com>
2021-01-23 10:18:30 +0100

    Adding a test

--- src/test/java/com/exemple/ThemeParkRideApplicationTests.java
+++ src/test/java/com/exemple/ThemeParkRideApplicationTests.java
@@ -3,13 +3,18 @@ package com.exemple;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class ThemeParkRideApplicationTests {
 
 	 @Autowired
